f4e41d4ee163b405b52f10285ecbd7907a7ecebf
kevinqtogitty <103936802+kevinqtogitty@users.noreply.github.com>
2023-03-07 16:35:27 -0500

    Update: added card details to sidebar

--- src/components/GeneratedPrompt.tsx
+++ src/components/GeneratedPrompt.tsx
@@ -21,7 +21,8 @@ const GeneratedPrompt: React.FC<Props> = ({
   isPromptOpen,
   question,
   chosenCards,
-  spreadChoice
+  spreadChoice,
+  setIsPromptOpen
 }) => {
   const transitionRef = useSpringRef();
   const transitionOverlay = useTransition(isPromptOpen, {
@@ -34,37 +35,56 @@ const GeneratedPrompt: React.FC<Props> = ({
   const springRef = useSpringRef();
   const spring = useSpring({
     ref: springRef,
-    x: isPromptOpen ? '25%' : '75%'
+    x: isPromptOpen ? '0%' : '50%'
   });
-
-  console.log(chosenCards);
 
   useChain([transitionRef, springRef], [0, 1], 100);
   return transitionOverlay(
     (style, item) =>
       item && (
         <a.div style={style} className="generated-prompt">
-          <a.div style={spring}>
-            My question: {question}
-            <br />
-            My spread:{' '}
-            {spreadChoice === 1
-              ? '3 card spread'
-              : spreadChoice === 2
-              ? '5 card spread'
-              : spreadChoice === 3
-              ? '10 card eltic spread'
-              : null}
-            <br />
-            Please give a reading based on the following chosen cards and my
-            question above:
-            <ol>
+          <a.div style={spring} className="prompt-container">
+            <h2>Prompt</h2>
+            <p>My question: {question}</p>
+            <p>
+              My spread:{' '}
+              {spreadChoice === 1
+                ? '3 card spread'
+                : spreadChoice === 2
+                ? '5 card spread'
+                : spreadChoice === 3
+                ? '10 card eltic spread'
+                : null}
+            </p>
+            <p>
+              Please give a reading based on the following chosen cards and my
+              question above:
+            </p>
+            <ol className="chosen-cards-list">
               {chosenCards.map((card, i) => (
                 <li key={i}>
                   {card.name} {card.reversed ? 'reversed' : 'not reversed'}
                 </li>
               ))}
             </ol>
+            <div>
+              <h2>Key</h2>
+              <ul className="card-key">
+                {chosenCards.map((card, i) => (
+                  <li key={i}>
+                    <h3>{card.name}</h3>
+                    <p>
+                      <span>Upright: </span>
+                      {card.meaning.upright}
+                    </p>
+                    <p>
+                      <span>Reversed: </span>
+                      {card.meaning.reversed}
+                    </p>
+                  </li>
+                ))}
+              </ul>
+            </div>
           </a.div>
         </a.div>
       )
